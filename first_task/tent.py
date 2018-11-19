bf88c16b2784d520c8690da958588e91f7037cc6
SergeiShumilin <NoiSD@yandex.ru>
2018-11-19 20:10:13 +0300

    first commit

--- first_task/tent.py
+++ first_task/tent.py
@@ -1,13 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
 
-
-def parse_ext2():
-    from bs4 import BeautifulSoup
-    import requests
+def parse_sitemap():
     page = requests.get('https://chrome.google.com/webstore/sitemap')
     soup = BeautifulSoup(page.content, 'xml')
-    titles = soup.find_all('sitemap')
-    for title in titles:
-        print(title.get_text())
+    for url in soup.find_all('loc'):
+        extract_ext(url.text)
+
+def extract_ext(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'xml')
+    for url in soup.find_all('loc'):
+        print(url.text)
+
+def ext_info(url):
+    pass
 
 
-parse_ext2()
+parse_sitemap()
