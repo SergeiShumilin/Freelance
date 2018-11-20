264a85667e595439c59b5dea3244aa084cd92e53
SergeiShumilin <NoiSD@yandex.ru>
2018-11-20 20:16:48 +0300

    counting of users added

--- first_task/tent.py
+++ first_task/tent.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import re
+from selenium import webdriver
 
 def parse_sitemap():
     page = requests.get('https://chrome.google.com/webstore/sitemap')
@@ -17,20 +19,35 @@ def ext_info(url):
     """
     Extract info about particular extension
 
-    Extracts four main features: name, number of users, number of references, rank
+    Extracts four main features: name, number of users, number of comments, rank
 
     :param url: extension's web page
 
     """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html5lib')
-    name = soup.find('h1', class_='e-f-w').get_text()
-    rank = soup.find('span', class_='q-N-nd')['aria-label']
+
+    name = soup.find('h1', class_='e-f-w').get_text() #gets the ext's name
+
+    rank = get_rank(soup.find('span', class_='q-N-nd')['aria-label']) #gets the ext's rank
+
+    users = get_users(soup.find('span', class_='e-f-ih')['title']) #gets the ext's users
+
+    comments = soup.find_all('div',  {'class': 'ba-bc-Xb ba-ua-zl-Xb'})
+    print(comments)
+
+    print('Name: '+ name + '\nRank: ' + str(rank)+ '\nUsers: ' + str(users))
+
+def get_users(info):
+    match = re.findall(r'(\d*)', info)
+    return int(''.join(match))
 
 
-    print(rank)
-    print(name)
 
+
+def get_rank(info):
+    match = re.search(r'(\d\.\d)',info)
+    return float(match.group())
 
 
 
