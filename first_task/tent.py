1dfd2b2a7db682cb7fba4e055d0eb6dcc8a8ac66
SergeiShumilin <NoiSD@yandex.ru>
2018-11-22 18:59:31 +0300

    what to do

--- first_task/tent.py
+++ first_task/tent.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import scrapy
 
 def parse_sitemap():
     page = requests.get('https://chrome.google.com/webstore/sitemap')
@@ -32,7 +33,7 @@ def ext_info(url):
 
     users = get_users(soup.find('span', class_='e-f-ih')['title']) #gets the ext's users
 
-    reviews(url)
+    reviews = get_reviews(url)
 
     print('Name: '+ name + '\nRank: ' + str(rank)+ '\nUsers: ' + str(users))
 
@@ -40,34 +41,20 @@ def get_users(info):
     match = re.findall(r'(\d*)', info)
     return int(''.join(match))
 
-def reviews(url):
-    from selenium import webdriver
-    from selenium.webdriver.common.by import By
-    from selenium.webdriver.support.ui import WebDriverWait
-    from selenium.webdriver.support import expected_conditions as EC
-    import time
+def get_reviews(url):
+    pass
 
-    driver = webdriver.Chrome()
 
-    driver.get('https://chrome.google.com/webstore/detail/evernote-web-clipper/pioclpoplcdbaefihamjohnefbikjilc?hl=en')
-    wait = WebDriverWait(driver, 5)
-    wait.until(EC.visibility_of_element_located((By.ID, ':21'))).click()
-    wait.until(
-        EC.visibility_of_element_located((By.CSS_SELECTOR, '.h-z-Ba-ca.ga-dd-Va.g-aa-ca'))
-    ).click()
 
-    english = driver.find_element_by_xpath('//div[@class="ah-mg-j"]/span').text
-    print('English: ' + english.split()[-1])
 
-    wait.until(
-        EC.visibility_of_element_located((By.XPATH, '//div[@class="g-aa-ca-ma-x-L" and text() = "All languages"]'))
-    ).click()
-    wait.until_not(EC.text_to_be_present_in_element((By.XPATH, '//div[@class="ah-mg-j"]/span'), english))
-    time.sleep(2)
 
-    AllCount = driver.find_element_by_xpath('//div[@class="ah-mg-j"]/span').text
-    print('All languages: ' + AllCount.split()[-1])
-    driver.close()
+
+
+
+
+
+
+
 
 
 def get_rank(info):
