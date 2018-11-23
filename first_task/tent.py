63419f578bff55b9ff1f163458c5e483d4cc130a
SergeiShumilin <NoiSD@yandex.ru>
2018-11-23 20:02:53 +0300

    what to do

--- first_task/tent.py
+++ first_task/tent.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import scrapy
+from selenium import webdriver
 
 def parse_sitemap():
     page = requests.get('https://chrome.google.com/webstore/sitemap')
@@ -29,6 +30,13 @@ def ext_info(url):
 
     name = soup.find('h1', class_='e-f-w').get_text() #gets the ext's name
 
+    driver = webdriver.Chrome()
+    driver.get(url)
+    html = driver.page_source
+    soup2 = BeautifulSoup(html,'html5lib')
+    print(soup2.find_all('div',class_='ba-bc-Xb ba-ua-zl-Xb'))
+
+
     rank = get_rank(soup.find('span', class_='q-N-nd')['aria-label']) #gets the ext's rank
 
     users = get_users(soup.find('span', class_='e-f-ih')['title']) #gets the ext's users
@@ -40,6 +48,10 @@ def ext_info(url):
 def get_users(info):
     match = re.findall(r'(\d*)', info)
     return int(''.join(match))
+
+def get_rank(info):
+    match = re.search(r'(\d\.\d)',info)
+    return float(match.group())
 
 def get_reviews(url):
     pass
@@ -57,9 +69,9 @@ def get_reviews(url):
 
 
 
-def get_rank(info):
-    match = re.search(r'(\d\.\d)',info)
-    return float(match.group())
+
+
+
 
 
 
