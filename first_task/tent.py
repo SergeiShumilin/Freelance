997cd64e188661b5d8ee8e74ef9c43ca56761e14
SergeiShumilin <noisd@yandex.ru>
2018-12-10 14:31:21 +0300

    amend

--- first_task/tent.py
+++ first_task/tent.py
@@ -1,19 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
-import re
 import pandas as pd
 import numpy as np
+import re
 
-
-def parse_sitemap():
-    main_df = pd.DataFrame(np.array([[0, 0, 0, 0, 0]]), columns=['Name', 'Users', 'Rank','Num ratings','Link'])
+def parse_sitemap(find_theme = False):
+    if find_theme:
+        main_df = pd.DataFrame(np.array([[0, 0, 0, 0, 0,0]]), columns=['Name', 'Users', 'Rank', 'Num ratings','Link',
+                                                                     'Description'])
+    else:
+        main_df = pd.DataFrame(np.array([[0, 0, 0, 0, 0]]), columns=['Name', 'Users', 'Rank','Num ratings','Link'])
 
     page = requests.get('https://chrome.google.com/webstore/sitemap')
     soup = BeautifulSoup(page.content, 'xml')
     i = 0
     for url in soup.find_all('loc'):
-        if i==50: break
-        df = extract_ext(url.text)
+        if i==2: break
+        df = extract_ext(url.text,find_theme)
         main_df = main_df.append(df, ignore_index=True)
         i+=1
 
@@ -23,43 +26,62 @@ def parse_sitemap():
 
 
 
-def extract_ext(url):
-    interm_df = pd.DataFrame(np.array([[0, 0, 0, 0, 0]]), columns=['Name', 'Users', 'Rank','Num ratings','Link'])
+def extract_ext(url, find_theme):
+
+    if get_theme:
+        interm_df = pd.DataFrame(np.array([[0, 0, 0, 0, 0, 0]]), columns=['Name', 'Users', 'Rank', 'Num ratings','Link',
+                                                                       'Description'])
+    else:
+        interm_df = pd.DataFrame(np.array([[0, 0, 0, 0, 0]]), columns=['Name', 'Users', 'Rank','Num ratings','Link'])
 
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'xml')
     i = 0
     for url in soup.find_all('loc'):
-        if i == 100000: break
-        df = ext_info(url.text)
+        if i == 100: break
+        df = ext_info(url.text,find_theme)
         interm_df = interm_df.append(df,ignore_index=True)
         i+=1
     return interm_df
 
 
-def ext_info(url):
+def ext_info(url, find_theme):
     """
     Extract info about particular extension
 
     Extracts four main features: name, number of users, number of comments, rank
 
     :param url: extension's web page
+    :param find_theme: bool
 
     """
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html5lib')
-
     name = get_name(soup)
+    if find_theme:
+        if bool(re.search(r'[Tt]heme', name)):
+            return get_theme(name, soup, url)
+    else:
+        return get_ext(name, soup, url)
 
+
+def get_ext(name, soup, url):
     rank = get_rank(soup)
-
     users = get_users(soup)
-
     ratings = get_ratings(soup)
+    print('extns')
+    return pd.DataFrame([[name, users, rank, ratings, url]], columns=['Name', 'Users', 'Rank', 'Num ratings', 'Link'])
 
 
-    print('Name: '+ name  + '\nRank: ' + str(rank)+ '\nUsers: ' + str(users)+'\nNum ratings: ' + str(ratings)+'\nLink: ' + url)
-    return pd.DataFrame([[name, users, rank,ratings,url]], columns=['Name', 'Users', 'Rank','Num ratings','Link'])
+
+def get_theme(name, soup, url):
+    rank = get_rank(soup)
+    users = get_users(soup)
+    ratings = get_ratings(soup)
+    description = get_description(soup)
+    print('theme')
+    return pd.DataFrame([[name, users, rank, ratings, url, description]],
+                        columns=['Name', 'Users', 'Rank', 'Num ratings', 'Link', 'Description'])
 
 def get_name(soup):
     name_tag = soup.find('h1', class_='e-f-w') #gets the ext's name
@@ -95,11 +117,21 @@ def get_ratings(soup):
         return int(ratings.get_text()[1:-1])
     else: return 0
 
+def get_description(soup):
+    desc_tag = soup.find('pre', class_='C-b-p-j-Oa') #gets the ext's desc
+    short_desc = soup.find('div', class_='C-b-p-j-Pb') # short description
+    if (desc_tag is not None) & (short_desc is not None):
+        return short_desc.get_text() + desc_tag.get_text()
+    elif desc_tag is not None:
+        return desc_tag.get_text()
+    elif short_desc is not None:
+        return short_desc.get_text()
+    else: return ''
 
 
 
+parse_sitemap(find_theme=True)
 
-parse_sitemap()
 
-
-#ext_info('https://chrome.google.com/webstore/detail/evernote-web-clipper/pioclpoplcdbaefihamjohnefbikjilc?hl=en')
+#ext_info('https://chrome.google.com/webstore/detail/foodie/hlkgmeebmcmbbfoaamicfcljhcidkdof'
+#         ,find_theme=True)
--- first_task/work_with_extensions.py
+++ first_task/work_with_extensions.py
@@ -1,7 +1,14 @@
-import pandas as pd
 
-df = pd.read_csv('extensions.csv',index_col=0)
-df = df.sort_values(['Users','Rank'],ascending=[False,False])
-df.dropna()
-df = df.reset_index(drop=True)
-df.to_csv('extensions.csv')
+
+
+def refine_table(table, drop_Descr = False):
+    import pandas as pd
+    df = pd.read_csv(table, index_col=0)
+    df = df.sort_values(['Users', 'Rank'], ascending=[False, False])
+    df.dropna()
+    df = df.reset_index(drop=True)
+    if drop_Descr:
+        df = df.drop(['Description'], axis=1)
+    df.to_csv('themes.csv')
+
+refine_table('extensions.csv')
